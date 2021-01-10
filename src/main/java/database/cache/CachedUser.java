dd9c196b62ef3f6111601504ac2cff25d2ab1c8a
FunkFrog <frdyttedf1@gmail.com>
2021-01-10 17:36:41 -0600

    Thin the cached user to what it needs to be

--- src/main/java/database/cache/CachedUser.java
+++ src/main/java/database/cache/CachedUser.java
@@ -8,25 +8,17 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class CachedUser {
-    private String username;
-    private int globalwarns;
     private Boolean blacklist;
     private long XP;
-    private int level;
     private long credits;
-    private boolean botauth;
     public boolean cachedSuccessfully;
 
     public CachedUser (User user) {
         try {
             ResultSet userData = UserData.getDBRow(user.getId());
-            this.username = userData.getString("username");
-            this.globalwarns = userData.getInt("globalwarns");
             this.blacklist = userData.getBoolean("blacklist");
             this.XP = userData.getLong("XP");
-            this.level = userData.getInt("level");
             this.credits = userData.getLong("credits");
-            this.botauth = userData.getBoolean("botauth");
             cachedSuccessfully = true;
         } catch(SQLException e) {
             SysLog.err("Couldn't retrieve data to cache for user " + user.getDiscriminatedName()
@@ -38,14 +30,6 @@ public class CachedUser {
         return cachedSuccessfully;
     }
 
-    public String getUsername() {
-        return username;
-    }
-
-    public int getGlobalwarns() {
-        return globalwarns;
-    }
-
     public Boolean isBlacklist() {
         return blacklist;
     }
@@ -54,15 +38,7 @@ public class CachedUser {
         return XP;
     }
 
-    public int getLevel() {
-        return level;
-    }
-
     public long getCredits() {
         return credits;
-    }
-
-    public boolean isBotauth() {
-        return botauth;
     }
 }
